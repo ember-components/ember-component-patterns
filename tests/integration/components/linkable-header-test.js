fe93a67d7e8960fa9a5bf1fdb55f3a981cfbb90c
Matt James <matt.james@secondstreet.com>
2020-12-17 10:55:28 -0600

    :sparkles: Add linkable header component

---
+++ tests/integration/components/linkable-header-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | linkable-header', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<LinkableHeader />`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+    <LinkableHeader @linkTitle="For Developers" />
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'For Developers');
+  });
+});
