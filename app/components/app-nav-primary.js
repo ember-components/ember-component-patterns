8eb6850c60b0d7fac8e8cac177742dd4c7327f7a
Ava Wroten <ava@wroten.me>
2021-02-03 14:31:10 -0600

    More responsive, better a11y

--- app/components/app-nav-primary.js
+++ app/components/app-nav-primary.js
@@ -3,16 +3,34 @@ import { inject as service } from '@ember/service';
 
 export default class extends Component {
   @service store;
+  @service media;
 
   isLoading = true;
 
   constructor() {
     super(...arguments);
     this.loadContent();
+    this.handleKeydown = this.handleKeydown.bind(this);
+    document.addEventListener('keydown', this.handleKeydown);
+  }
+
+  get show() {
+    return !this.media.isMobile || this.media.isMobile && this.args.navPrimaryIsOpen;
   }
 
   loadContent() {
     this.contents = this.store.findAll('content');
     this.isLoading = false;
+  }
+
+  handleKeydown({ key }) {
+    if (this.args.navPrimaryIsOpen && key === 'Escape') {
+      this.args.toggleNavPrimary();
+    }
+  }
+
+  /* eslint-disable ember/require-super-in-lifecycle-hooks */ // I am calling super... lol
+  willDestroy() {
+    document.removeEventListener('keydown', this.handleKeydown);
   }
 }
